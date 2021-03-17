cac41e143180098b8b7a02adbbbf97446124fd6c
valgmez <valeriagomezramirez99@gmail.com>
2021-03-17 12:20:30 +0000

    added stratify
    working on cross-validation now

--- brain-age2.py
+++ brain-age2.py
@@ -9,7 +9,7 @@ from keras.models import Sequential, Input, Model
 from keras.layers import Input, Dense, Activation, BatchNormalization, Dropout, Flatten, Conv3D, MaxPooling3D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import ReLU
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import confusion_matrix
 from keras.utils import plot_model
 import keras.backend as K
@@ -45,7 +45,7 @@ mris = np.load('/nfshome/store01/users/c.c1732102/mris.npy')
 # 80/20 train test split
 # reshape
 X_train, X_test, y_train, y_test = train_test_split(
-    mris, ages, test_size=0.2, random_state=13)
+    mris, ages, test_size=0.2, random_state=13, stratify=ages)  # added stratify for ages
 
 print("number of training examples = " + str(X_train.shape[0]))
 print("number of test examples = " + str(X_test.shape[0]))
@@ -57,8 +57,9 @@ print("y_test shape: " + str(y_test.shape))
 X_train = X_train.reshape(-1, 96, 112, 96, 1)
 X_test = X_test.reshape(-1, 96, 112, 96, 1)
 
-
 # building the model
+
+
 def BrainAgeModel(input_shape):
 
     # define the input placeholder as a tensor with shape input_shape
@@ -85,9 +86,9 @@ def BrainAgeModel(input_shape):
     X = MaxPooling3D((2, 2, 2), strides=2, padding='same')(X)
 
     # 5th layer CONV -> BN -> MaxPooling applied to X
-    # X = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(X)
-    # X = BatchNormalization(axis=4)(X)
-    # X = MaxPooling3D((2, 2, 2), strides=2, padding='same')(X)
+    X = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(X)
+    X = BatchNormalization(axis=4)(X)
+    X = MaxPooling3D((2, 2, 2), strides=2, padding='same')(X)
 
     # flatten X (convert it to a vector)
     X = Flatten()(X)
@@ -112,16 +113,11 @@ brainAgeModel = BrainAgeModel(X_train.shape[1:])
 
 # 2. compile model
 brainAgeModel.compile(
-    optimizer='adam', loss='mean_squared_error', metrics=['mse', 'mae', 'mape'])
+    optimizer='adam', loss='mean_squared_error', metrics=['mse', 'mae'])
 
 # 3. train the model
 history = brainAgeModel.fit(x=X_train, y=y_train, epochs=200,
                             batch_size=4, validation_data=(X_test, y_test))
-
-plt.plot(history.history['mse'])
-plt.plot(history.history['mae'])
-plt.plot(history.history['mape'])
-plt.show()
 
 # 4. evaluate the model
 test_eval = brainAgeModel.evaluate(x=X_test, y=y_test)
@@ -129,33 +125,28 @@ print()
 print('Loss: ', test_eval[0])
 print('Mean Squared Error: ', test_eval[1])
 print('Mean Absolute Error: ', test_eval[2])
-print('Mean Absolute Percentage Error: ', test_eval[3])
 
 
 def plot_results(brainAgeModel):
-    # model.compile(loss = 'mean_squared_error', optimizer = 'adam', metrics = ['mse', 'mae'])
-    # trained_model = model.fit(X_train, y_train, batch_size = batch_size, epochs = epochs, validation_data = (X_test, y_test))
     mse = brainAgeModel.history['mse']
     val_mse = brainAgeModel.history['val_mse']
     mae = brainAgeModel.history['mae']
     val_mae = brainAgeModel.history['val_mae']
     loss = brainAgeModel.history['loss']
     val_loss = brainAgeModel.history['val_loss']
-    mape = brainAgeModel.history['mape']
-    val_mape = brainAgeModel.history['val_mape']
     epochs = range(len(mse))
 
-    plt.plot(epochs, mse, 'bo', label='Training MSE')
-    plt.plot(epochs, val_mse, 'b', label='Val MSE')
-    plt.title('Mean Square Error (MSE)')
+    plt.plot(epochs, mse, label='Training MSE')
+    plt.plot(epochs, val_mse, label='Val MSE')
+    plt.title('Mean Squared Error (MSE)')
     plt.xlabel('Epochs')
     plt.ylabel('MSE')
     plt.legend()
     plt.savefig('mris_mse.png')
 
     plt.figure()
-    plt.plot(epochs, mae, 'bo', label='Training MAE')
-    plt.plot(epochs, val_mae, 'b', label='Validation MAE')
+    plt.plot(epochs, mae, label='Training MAE')
+    plt.plot(epochs, val_mae, label='Validation MAE')
     plt.title('Mean Absolute Error (MAE)')
     plt.xlabel('Epochs')
     plt.ylabel('MAE')
@@ -163,22 +154,13 @@ def plot_results(brainAgeModel):
     plt.savefig('mris_mae.png')
 
     plt.figure()
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.plot(epochs, loss, label='Training loss')
+    plt.plot(epochs, val_loss, label='Validation loss')
     plt.title('Loss evaluation')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
     plt.savefig('mris_loss.png')
-
-    plt.figure()
-    plt.plot(epochs, mape, 'bo', label='Training MAPE')
-    plt.plot(epochs, val_mape, 'b', label='Validation MAPE')
-    plt.title('Mean Absolute Percentage Error (MAPE)')
-    plt.xlabel('Epochs')
-    plt.ylabel('MAPE')
-    plt.legend()
-    plt.savefig('mris_mape.png')
 
 
 plot_results(history)
